93c2a6be3a66c5087a65bad53a4a092ca7b1b4ee
Lazar V. Todorovic <lazar.todorovic@jack.agency>
2024-01-04 16:38:53 +0100

    protected route

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,16 +1,26 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+
 import Home from '../pages/Home'
 import SignIn from '../pages/SignIn'
 import Profile from '../pages/Profile'
 
+function ProtectedRoute({ component }) {
+    const user = useSelector((state) => state.user)
+    return user && user.isLoggedIn ? component : <Navigate to="/signin" />
+}
 
-function AppRouter ()  {
+function AppRouter() {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/signin" element={<SignIn />} />
-                <Route path="/profile" element={<Profile />} />
+                <Route path="/profile" element={
+                    <ProtectedRoute>
+                        <Profile />
+                    </ProtectedRoute>
+                }/>
             </Routes>
         </Router>
     )
